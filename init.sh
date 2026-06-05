43bb34a0bc10ccbeb8eab9bb302d9fc5eb1fed1b
jmoser2004 <joe@joemoser.dev>
2026-06-05 00:42:32 -0400

    Shrunk download size and improved init.sh with systemd

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir dummy_data
+dd if=/dev/zero of=dummy_data/ten_meg.bin bs=1M count=10
 dd if=/dev/zero of=dummy_data/quarter_gig.bin bs=1M count=256
 dd if=/dev/zero of=dummy_data/half_gig.bin bs=1M count=512
 dd if=/dev/zero of=dummy_data/gig.bin bs=1M count=1024
@@ -7,3 +8,6 @@ dd if=/dev/zero of=dummy_data/gig.bin bs=1M count=1024
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+
+sudo cp curl-st.service /etc/systemd/system/curl-st.service
+sudo systemctl start curl-st.service
