6777f8fa29db98dfc62bbf040cab8f2aba9b1bf3
jmoser2004 <joe@joemoser.dev>
2026-06-04 22:31:45 -0400

    Minor init changes

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@ mkdir dummy_data
 dd if=/dev/zero of=dummy_data/quarter_gig.bin bs=1M count=256
 dd if=/dev/zero of=dummy_data/half_gig.bin bs=1M count=512
 dd if=/dev/zero of=dummy_data/gig.bin bs=1M count=1024
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
